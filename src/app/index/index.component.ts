890b5bad658cc19182fc965da24033794da0f5e6
EternaLight <EternaLight98@outlook.com>
2024-02-09 02:28:30 -0500

    V2.0

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
+
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [ RouterLink, CommonModule],
+  imports: [ RouterLink, CommonModule,],
   templateUrl: './index.component.html',
   styleUrl: './index.component.scss'
 })
