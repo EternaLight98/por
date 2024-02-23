c1602dbf0d0339b33a68ddc989457339336700b7
EternaLight <EternaLight98@outlook.com>
2024-02-23 00:50:26 -0500

    V3.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { DesarrolloComponent } from './desarrollo/desarrollo.component';
 import { IndexComponent } from './index/index.component';
 import { MarketingComponent } from './marketing/marketing.component';
+import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ContactoComponent } from './contacto/contacto.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IndexComponent, DesarrolloComponent, CommonModule, MarketingComponent],
+  imports: [RouterOutlet, IndexComponent, DesarrolloComponent, CommonModule, MarketingComponent, NosotrosComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { IndexComponent } from './index/index.component'; // Add missing import
 import { EdicionComponent } from './edicion/edicion.component';
 import { MarketingComponent } from './marketing/marketing.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
-
+import { ContactoComponent } from './contacto/contacto.component';
 
 export const routes: Routes = [
   {path: '', component: IndexComponent},
@@ -13,4 +13,5 @@ export const routes: Routes = [
   {path: 'edicion', component: EdicionComponent },
   {path: 'marketing', component: MarketingComponent},
   {path: 'nosotros', component: NosotrosComponent},
+  {path: 'contacto', component: ContactoComponent},
 ];
